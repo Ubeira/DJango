8575d06b5baf8bbc8622c3470383e4fa334d73c2
Ubeira <ubeiram@gmail.com>
2021-05-28 11:52:50 +0200

    plantillas2

--- plantillas/plantillas/urls.py
+++ plantillas/plantillas/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from plantillas.views import hello, helloCtx
+from plantillas.views import clasesData, hello, helloCtx, actualTime
 from django.contrib import admin
 from django.urls import path
 
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello),
     path('helloCtx/', helloCtx),
+    path("", actualTime),
+    path("classes", clasesData)
 ]
--- plantillas/plantillas/views.py
+++ plantillas/plantillas/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.http import HttpResponse
 from django.template import Template, Context
 
@@ -25,4 +26,34 @@ def helloCtx(request):
 # 4º renderizamos
     documento = plt.render(ctx)
 
+    return HttpResponse(documento)
+
+
+def actualTime(request):
+    time = datetime.now()
+    doc_externo = open(
+        "C:/Users/NITROPC/Documents/My-Code/pildoras/python/DJango/plantillas/plantillas/plantillasHtml/time.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context({"time": time})
+    documento = plt.render(ctx)
+    return HttpResponse(documento)
+
+
+class Persona(object):
+
+    def __init__(self, nombre, apellido) -> None:
+        self.nombre = nombre
+        self.apellido = apellido
+
+
+def clasesData(request):
+
+    p1 = Persona("Don Iago", "Ubeira")
+    doc_externo = open(
+        "C:/Users/NITROPC/Documents/My-Code/pildoras/python/DJango/plantillas/plantillas/plantillasHtml/class.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context({"nombre": p1.nombre, "apellido": p1.apellido})
+    documento = plt.render(ctx)
     return HttpResponse(documento)
